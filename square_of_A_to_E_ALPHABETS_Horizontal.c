62d88c5bf1dcbd6ab251bc491f49d3ec81b1ae92
suriya46 <42246346+suriya46@users.noreply.github.com>
2019-04-19 21:27:46 +0530

    Add files via upload

---
+++ square_of_A_to_E_ALPHABETS_Horizontal.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+char i,j;
+for(i='A';i<='E';i++)
+{
+
+    for(j='A';j<='E';j++)
+{
+    printf("%c\t",i);
+}
+    printf("\n");
+
+}
+}
---
+++ square_of_A_to_E_ALPHABETS_Vertical.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+char i,j;
+for(i='A';i<='E';i++)
+{
+
+    for(j='A';j<='E';j++)
+{
+    printf("%c\t",j);
+}
+    printf("\n");
+
+}
+}
+
